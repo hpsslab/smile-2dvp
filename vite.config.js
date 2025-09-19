6fe77edd4765424a6cb153b77b15cc092c16980e
Tristan Yan <yantristan3001@gmail.com>
2025-09-18 19:52:56 -0600

    config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const repoName = 'smile-mvp';
+
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/',
 })
